cc47963fe5f0d33752a71f82416e14db4d56eb2d
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-09-12 21:31:45 -0400

    Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,18 +1,13 @@
-# Uses input file 1
 import sys
 import os
 import math
 
-print('got here')
-filepath = sys.argv[1]
+filepath = 'C:\\Users\\Allen\\Desktop\\input1.txt'
 
 total = 0
 with open(filepath) as fp:
     cnt = 0
     for line in fp:
-        num = float(line)
-        num = math.floor(num / 3)
-        num = num - 2
-        total =  total + num
+        total += math.floor(int(line)/3) - 2
 
 print(total)
