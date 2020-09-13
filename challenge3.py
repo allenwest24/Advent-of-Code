a4f7be3f19a4685a561421b8f995a0e604a93039
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-09-12 22:19:59 -0400

    Update challenge3.py

--- challenge3.py
+++ challenge3.py
@@ -2,40 +2,14 @@ import sys
 import os
 import math
 
-filepath = sys.argv[1]
-
-total = 0
-with open(filepath) as fp:
-    for line in fp:
-        currentline = line.split(",")
-        myarray = []
-        ii = 0
-        while ii < 137:
-            myarray.append(currentline[ii])
-            ii += 1
-        print('array created')
-    myarray[1] = 12
-    myarray[2] = 2
-    jj = 0
-    temp1 = 0
-    temp2 = 0
-    temp3 = 0
-    temp4 = 0
-    while jj < 138:
-        temp1 = int(myarray[jj])
-        temp2 = int(myarray[jj + 1])
-        temp3 = int(myarray[jj + 2])
-        temp4 = int(myarray[jj + 3])
-        if temp1 == 1:
-            temp1 = int(myarray[temp2]) + int(myarray[temp3])
-            myarray[temp4] = temp1
-            jj += 4
-        elif temp1 == 2:
-            temp1 = int(myarray[temp2]) * int(myarray[temp3])
-            myarray[temp4] = temp1
-            jj += 4
-        elif temp1 == 99:
-            print(myarray)
-            break
-        else:
-            print('error')
+array = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0]
+ii = 0
+while ii < len(array):
+    if array[ii] == 1:
+        array[array[ii+3]] = array[array[ii+2]] + array[array[ii+1]]
+    elif array[ii] == 2:
+        array[array[ii+3]] = array[array[ii+2]] * array[array[ii+1]]
+    else:
+        print(array[0])
+        break
+    ii += 4
