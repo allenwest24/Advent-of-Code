cc26407a52e36a0e980f34716a6bf77fcde4e8fc
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-12-02 00:16:38 -0500

    Create solution.go

---
+++ 2021/Day 02/Puzzle 1/solution.go
@@ -0,0 +1,47 @@
+package main
+
+// List of needed imports.
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Main method for this solution.
+func main() {
+	// Reading in the input file.
+	f, err := os.Open("day2puzzle1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	// Solve the puzzle.
+	totalDistance := 0
+	totalAltitude := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, " ")
+		direction := split[0]
+		amount, _ := strconv.Atoi(split[1])
+		if direction == "up" {
+			totalAltitude -= amount
+		} else if direction == "down" {
+			totalAltitude += amount
+		} else if direction == "forward" {
+			totalDistance += amount
+		}
+	}
+	location := totalDistance * totalAltitude
+	// Print the results.
+	fmt.Print(location)
+
+	// Checking for any errors that occurred.
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
