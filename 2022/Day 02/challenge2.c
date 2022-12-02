bdc57bafde69ab50d0a3c9546c1e6a4df800bb5a
Allen West <58156836+allenwest24@users.noreply.github.com>
2022-12-02 12:07:56 -0500

    Create challenge2.c

---
+++ 2022/Day 02/challenge2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Constants.
+#define MAX_LINES 4096
+#define MAX_LEN 1024
+
+// Main method.
+int main(void) {
+        // Create static size array.
+        char input_data[MAX_LINES][MAX_LEN];
+
+        // Open and read the file.
+        FILE *file;
+        file = fopen("input.txt", "r");
+        if (file == NULL) {
+                printf("Could not open file.\n");
+                return 1;
+        }
+
+        // Store the input data in the array we created.
+        int line = 0;
+        while (!feof(file) && !ferror(file)) {
+                if (fgets(input_data[line], MAX_LEN, file) != NULL) {
+                        line++;
+                }
+        }
+
+        // Close the input file.
+        fclose(file);
+
+        int total = 0;
+        // Do work here.
+        for (int ii = 0; ii < line; ii++) {
+                char foe = input_data[ii][0];
+                char me = input_data[ii][2];
+                if (foe == 'A') {
+                        if (me == 'X') {
+                                total += 0 + 3;
+                        }
+                        else if (me == 'Y') {
+                                total += 3 + 1;
+                        }
+                        else if (me == 'Z') {
+                                total += 6 + 2;
+                        }
+                }
+                else if (foe == 'B') {
+                        if (me == 'X') {
+                                total += 0 + 1;
+                        }
+                        else if (me == 'Y') {
+                                total += 3 + 2;
+                        }
+                        else if (me == 'Z') {
+                                total += 6 + 3;
+                        }
+                }
+                else if (foe == 'C') {
+                        if (me == 'X') {
+                                total += 0 + 2;
+                        }
+                        else if (me == 'Y') {
+                                total += 3 + 3;
+                        }
+                        else if (me == 'Z') {
+                                total += 6 + 1;
+                        }
+                }
+        }
+
+        printf("%d", total);
+
+        return 0;
+}
